1f7ad4372c00f8ef115a45837635d5d8575af999
Sukruti Mogili <sukrutimogili@gmail.com>
2026-06-10 19:51:21 +0530

    Add Groq and Calendar constants

---
+++ utils/constants.js
@@ -0,0 +1,6 @@
+export const GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions";
+export const GROQ_MODEL = "llama3-70b-8192";
+
+export const CALENDAR_API_BASE = "https://www.googleapis.com/calendar/v3";
+
+export const DEFAULT_REMINDERS = [7, 6, 5, 4, 3, 2, 1];
